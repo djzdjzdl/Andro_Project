b9051212eafccfb38259b730ef4f83b9b72a7140
djzdjzdl <42856018+djzdjzdl@users.noreply.github.com>
2018-11-28 21:43:21 +0900

    Update MainActivity.java
    이게 진짜

--- app/src/main/java/e/a2727/project/MainActivity.java
+++ app/src/main/java/e/a2727/project/MainActivity.java
@@ -4,21 +4,30 @@ package e.a2727.project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private Button btnLogout;
+    private LinearLayout linearlayout1, linearlayout2, linearlayout3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linearlayout1 = (LinearLayout)findViewById(R.id.linearLayout1);
+        linearlayout2 = (LinearLayout)findViewById(R.id.linearLayout2);
+        linearlayout3 = (LinearLayout)findViewById(R.id.linearLayout3);
         btnLogout = (Button) findViewById(R.id.btnLogout);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +38,43 @@ public class MainActivity extends AppCompatActivity {
         });
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        linearlayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "강의 클릭됨", LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                //강의 class 구현 시 풀어주세요
+//                startActivity(new Intent(MainActivity.this, gangui.class));
+//                finish();
+            }
+        });
+
+        linearlayout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "게시판 클릭됨", LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                //게시판 class 구현 시 풀어주세요
+//                startActivity(new Intent(MainActivity.this, board.class));
+//                finish();
+            }
+        });
+
+        linearlayout3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "지도 클릭됨", LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                //지도 class 구현 시 풀어주세요
+//                startActivity(new Intent(MainActivity.this, maps.class));
+//                finish();
+            }
+        });
+
     }
 
     private void Logout() {
@@ -37,4 +83,3 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, StartActivity.class));
     }
 }
-
